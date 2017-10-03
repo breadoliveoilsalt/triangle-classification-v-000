49eaec836260fe953a7f34707a928679bc938a19
Tony Distinti <adistinti@gmail.com>
2017-10-03 13:55:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,7 @@ class Triangle
   def kind
     if (@side1 || @side2 || @side3 < 1) || (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1)
 #      begin
+    binding.pry
         raise TriangleError
 #      rescue TriangleError => error
 #        puts error.message
