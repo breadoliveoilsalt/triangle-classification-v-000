aefffae308223cf3523153ab24473438bc4a1d80
Tony Distinti <adistinti@gmail.com>
2017-10-03 13:45:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,32 @@
 class Triangle
   # write code here
 
-
+  attr_accessor :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
   end
 
+  def kind
+    if (@side1 || @side2 || @side3 < 1) || (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1)
+#      begin
+        raise TriangleError
+#      rescue TriangleError => error
+#        puts error.message
+
+    elsif @side1 && @side2 == @side 3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 #relying on order of the conditionals here
+      :isosceles
+    elsif @side1 && @side2 != @side3
+      :scelene
+  end
+  
+end
+
+class TriangleError < StandardError
   
 end
